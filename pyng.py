3aa08b3e14dd406931bd8e0949855eb71afe9f1a
Jacob Willis <willisjp@dukes.jmu.edu>
2022-03-28 09:58:27 -0400

    Code
    New code

--- pyng.py
+++ pyng.py
@@ -1,3 +1,5 @@
+# Pyng
+# Find other devices on local network using MacOS
 import pythonping, os
 
 # Global Variables
@@ -7,5 +9,24 @@ ip_info = f'ifconfig | cat > {info}'
 # Commands
 os.system(ip_info)
 
+# Read in network info from ip config file
+temp = open(info)
+networks = temp.readlines()
+temp.close()
+
+count = 0
+for item in networks:
+	count += 1
+	if item[:4] == 'en0:':
+		break
+
+network = networks[count+3]
+address = network.split(" ")
+ip = address[1]
+print(ip)
+
+subnets = ip.split('.')
+for net in subnets:
+	print(net)
 
 
